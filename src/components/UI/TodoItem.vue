10aedcc4d5156d72ee7020bfbdb6a2167c2b6d52
Andry <muha_pk@mail.ru>
2023-11-11 00:04:06 +0100

    add tilewind, edit, date

--- src/components/UI/TodoItem.vue
+++ src/components/UI/TodoItem.vue
@@ -1,50 +1,51 @@
 <script setup>
-  import {defineProps, defineEmits} from "vue"
+import {defineProps, defineEmits, ref} from "vue"
+  import moment from "moment"
 
-  const emit = defineEmits(["remove", "done"])
+  const emit = defineEmits(["remove", "done", "edit"])
 
   const emitRemove = id => emit("remove", id)
-
   const emitDone = id => emit("done", id)
+  const emitEdit = (id, value) => emit("edit", id, value)
 
+  const inputValue = ref('');
+  const inputId = ref(false);
+  const changeInputId = id => inputId.value = id
+  const resetInputId = () => inputId.value = false
 
   const props = defineProps(['item'])
 
+  const dateFormat = moment(props?.item?.date).format('DD.MM.YYYY')
 
 </script>
 
 <template>
 
-  <div class="wrapper">
-    <div class="task" :class="props.item.status === 2 && 'done'">{{ props.item.id }}: <strong>{{ props.item.name }}</strong> {{props.item.description}}</div>
-    <button @click="emitRemove(props.item.id)">&times;</button>
+  <div class="flex items-center">
 
-    <input @click="emitDone(props.item.id)" type="checkbox" name="vehicle1" value="Bike">
+    <div v-show="!!inputId" class="flex w-full my-0.5">
+      <input
+          class="shadow text-xs appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="text"
+          name="input"
+          v-model="inputValue"
+      >
+      <button class="bg-red-600 text-xs hover:bg-red-700 text-white font-bold px-2 ml-1 rounded focus:outline-none focus:shadow-outline" @click="emitEdit(props.item.id, inputValue); resetInputId();">save</button>
+    </div>
+
+    <div v-show="!inputId" class="mr-auto text-sm" :class="props.item.status === 2 && 'line-through text-slate-300'">
+      {{ props.item.id }}:
+      <strong>{{ props.item.name }}</strong>
+      {{props.item.description}}
+    </div>
+    <span v-show="!inputId" class="text-xs mr-2">{{dateFormat}}</span>
+
+    <input v-show="!inputId" @click="emitDone(props.item.id)" type="checkbox">
+    <button v-show="!inputId" class="text-xs text-white font-bold px-1 ml-1 rounded focus:outline-none focus:shadow-outline" @click="changeInputId(props.item.id)">
+      <svg class="h-4 w-4 text-red-500" width="24"  height="24"  viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" /></svg>
+    </button>
+    <button v-show="!inputId" class="bg-red-600 text-xs hover:bg-red-700 text-white font-bold px-1 ml-1 rounded focus:outline-none focus:shadow-outline" @click="emitRemove(props.item.id)">&times;</button>
 
   </div>
 </template>
-
-<style>
-
-.wrapper {
-  display: flex;
-  justify-content: end;
-  width: 300px;
-  text-align: left;
-  margin: 2px auto;
-}
-
-.task {
-  margin-right: auto;
-}
-
-button {
-  cursor: pointer;
-}
-
-.done {
-  text-decoration: line-through;
-  color: #ccc;
-}
-
-</style>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,7 +1,8 @@
 <script setup>
 
 import TodoItem from "../components/UI/TodoItem"
-
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 import {ref} from "vue";
 
 
@@ -11,7 +12,7 @@ const listStorageData = JSON.parse(listStorage)
 const inputValue = ref('');
 const textareaValue = ref('');
 const list = ref(listStorageData ? listStorageData : []);
-
+const date = ref(new Date());
 
 const filterOpen = () => list.value = listStorageData.filter((data) => data.status === 1)
 const filterDone = () => list.value = listStorageData.filter((data) => data.status === 2)
@@ -23,6 +24,7 @@ const add = () => {
     id: list.value.length + 1,
     name: inputValue.value,
     description: textareaValue.value,
+    date: date,
     status: 1,
   })
   inputValue.value = ""
@@ -32,6 +34,17 @@ const add = () => {
 
 const remove = id => {
   list.value = list.value.filter((obj) => obj.id !== id)
+  localStorage.setItem("list", JSON.stringify(list.value))
+}
+
+const edit = (id, value) => {
+  const obj = list.value.find((obj) => obj.id === id)
+  const index = list.value.indexOf(obj)
+
+  let newObj = obj
+  newObj.name = value
+  list.value.splice(index, 1, newObj)
+
   localStorage.setItem("list", JSON.stringify(list.value))
 }
 
@@ -53,37 +66,60 @@ const done = id => {
 
 <template>
 
-  <div>
+  <div class="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-10">
 
-    <h1>Todo</h1>
+    <button class="bg-blue-500 text-xs hover:bg-blue-700 text-white font-bold py-1 px-2 mr-2 rounded focus:outline-none focus:shadow-outline" @click="filterOpen">Open</button>
+    <button class="bg-blue-500 text-xs hover:bg-blue-700 text-white font-bold py-1 px-2 mr-2 rounded focus:outline-none focus:shadow-outline" @click="filterDone">Done</button>
+    <button class="bg-blue-500 text-xs hover:bg-blue-700 text-white font-bold py-1 px-2 mr-2 rounded focus:outline-none focus:shadow-outline" @click="filterReset">Reset</button>
 
-    <button @click="filterOpen">Open</button>
-    <button @click="filterDone">Done</button>
-    <button @click="filterReset">Reset</button>
 
-    <div v-if="list.length">
-      <TodoItem v-for="item in list" :key="item.id" :item="item" @remove="remove" @done="done"></TodoItem>
+
+    <div v-if="list.length" class="mt-4">
+      <TodoItem v-for="item in list" :key="item.id" :item="item" @remove="remove" @done="done" @edit="edit"></TodoItem>
     </div>
 
     <div v-else>empty list </div>
 
-    <br />
+  </div>
 
-    <div class="form">
-      <input name="input" v-model="inputValue">
-      <textarea v-model="textareaValue" name="description" />
-      <button @click="add">add data</button>
-    </div>
+
+  <div class="w-full max-w-sm mx-auto">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+      <div class="mb-4">
+        <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="text"
+            name="input"
+            v-model="inputValue"
+        >
+      </div>
+
+      <div class="mb-4">
+        <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="description"
+            name="description"
+            v-model="textareaValue"
+        >
+      </div>
+
+      <div class="mb-4">
+        <VueDatePicker v-model="date"></VueDatePicker>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 mx-auto hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" @click="add">
+          Add data
+        </button>
+      </div>
+
+    </form>
 
   </div>
 
 </template>
 
-<style>
-.form {
-  display: flex;
-  flex-direction: column;
-  width: 300px;
-  margin: 0 auto;
-}
-</style>
+
